aac5fbd91f7b3a971981cfc5d2aedbc6fa882754
GermanDemidov <german.m.demidov@yandex.ru>
2015-03-11 16:41:32 +0300

    Added instances of transcription factors

---
+++ transcription_factor.cpp
@@ -0,0 +1,13 @@
+//
+//  transcription_factor.cpp
+//  trans_factors_distrib
+//
+//  Created by Герман Демидов on 11.03.15.
+//  Copyright (c) 2015 gdemidov. All rights reserved.
+//
+
+#include "transcription_factor.h"
+
+Transcription_factor::Transcription_factor(int id_of_tf, const std::vector<std::map<char, double>>& motif_of_tf): motif(motif_of_tf), id_of_current_tf(id_of_tf) {
+    
+}
---
+++ transcription_factor.h
@@ -0,0 +1,27 @@
+//
+//  transcription_factor.h
+//  trans_factors_distrib
+//
+//  Created by Герман Демидов on 11.03.15.
+//  Copyright (c) 2015 gdemidov. All rights reserved.
+//
+
+#ifndef __trans_factors_distrib__transcription_factor__
+#define __trans_factors_distrib__transcription_factor__
+
+#include <stdio.h>
+#include <vector>
+#include <map>
+
+class Transcription_factor {
+public:
+    Transcription_factor(int, const std::vector<std::map<char, double>>&);
+    double calculate_weight_of_binding(std::string);
+    void calculate_next_event();
+private:
+    bool binded_to_dna;
+    const int id_of_current_tf;
+    const std::vector<std::map<char, double>>& motif;
+};
+
+#endif /* defined(__trans_factors_distrib__transcription_factor__) */
